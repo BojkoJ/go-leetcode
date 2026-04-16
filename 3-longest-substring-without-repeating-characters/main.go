afe25a93caba9df5331407b9801a37d3e0bc16d6
Jan Bojko <honzabojko@seznam.cz>
2026-04-16 17:41:41 +0200

    styling

--- 3-longest-substring-without-repeating-characters/main.go
+++ 3-longest-substring-without-repeating-characters/main.go
@@ -6,10 +6,8 @@ import "fmt"
 // LeetCode: 3. Longest Substring Without Repeating Characters
 // Given a string s, find the length of the longest without duplicate characters.
 // Constraints:
-//
 //	0 <= s.length <= 5 * 104
 //	s consists of English letters, digits, symbols and spaces.
-//
 // ------------------------------------------------------------------
 // Example 1:
 // Input: s = "abcabcbb"
